a58c744217c93a7210421de5c795e1da65ad8bd3
sam19m <sam.dev.1729@gmail.com>
2023-09-13 13:10:54 +0330

    show file as list

---
+++ src/client.py
@@ -0,0 +1,38 @@
+"""Main."""
+import socket
+
+sock = socket.socket()
+print("Socket created successfully.")
+
+PORT = 8800
+HOST = "localhost"
+
+sock.connect((HOST, PORT))
+print("Connection Established.")
+data_list = sock.recv(1024).decode().split("-")
+data_list.pop(len(data_list) - 1)
+COUNTER = 1
+while True:
+    print("List of Files: ")
+    for index, value in enumerate(data_list):
+        print(index, ".", value)
+        print("-----------------------")
+
+    INDEX = str(input("\nchoose number of file you want to download: "))
+
+    sock.send(INDEX.encode())
+    recv_data = sock.recv(1024).decode()
+    file_name = recv_data + str(COUNTER)
+    with open(file_name, "wb") as file:
+        line = sock.recv(1024)
+        while line:
+            file.write(line)
+            line = sock.recv(1024)
+
+        print("File has been received successfully.")
+    COUNTER += 1
+    answer = input("you want to download again?(y/n)").lower()
+    if answer == "n":
+        print("Connection Closed.")
+        sock.close()
+        break
--- src/server.py
+++ src/server.py
@@ -1,28 +1,52 @@
 """Server."""
+
+import os
 import socket
 
 sock = socket.socket()
 print("Socket created successfully.")
-
 PORT = 8800
 HOST = ""
 
 sock.bind((HOST, PORT))
-
-sock.listen(10)
+sock.listen()
 print("Socket is listening...")
+# text = os.path.join("src", "assets", "text.txt")
+# video = os.path.join("src", "assets", "video.mp4")
+# image = os.path.join("src", "assets", "image.png")
+# sound = os.path.join("src", "assets", "sound.mp3")
+src = os.walk("src/assets")
+src_list = []
+for item in src:
+    for value in item[2]:
+        # print(idex)
+        src_list.append(os.path.join("src", "assets", value))
+
+
+def send(soc, data):
+    """Send."""
+    soc.send(str(data).encode())
+    # try:
+    #     soc.send(str(data).encode())
+    # except Exception:
+    #     soc.send("error to send data form sever!".encode())
+
 
 while True:
+    PATH = "".join(f"{value}-" for value in src_list)
+    # print(strr)
     client, addr = sock.accept()
     print("Connected with ", addr)
+    send(client, PATH)
 
-    data = client.recv(1024)
-    print(data.decode())
-    client.send("vide.mp4".encode())
-    with open(data.decode(), "rb") as file:
+    index = int(client.recv(1024).decode())
+    print(index)
+    file_name = os.path.basename(src_list[index])
+    send(client, file_name)
+    with open(src_list[index], "rb") as file:
         line = file.read(1024)
         while line:
-            client.send(line)
+            send(client, line)
             line = file.read(1024)
 
     print("File has been transferred successfully.")
