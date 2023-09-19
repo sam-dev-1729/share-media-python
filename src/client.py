3e8a2551a5fd33fa3a3cafc633a93d87d1686797
sam-dev-1729 <sam.dev.1729.com>
2023-09-18 23:27:21 -1000

    add GUI

--- src/client.py
+++ src/client.py
@@ -1,38 +1,90 @@
-"""Main."""
+"""Client."""
 import socket
+import tkinter as tk
+from tkinter import ttk
 
 print("Socket created successfully.")
 
 PORT = 8800
 HOST = "localhost"
 
+sock = socket.socket()
+sock.connect((HOST, PORT))
+print("Connection Established.")
 
-while True:
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    print("Connection Established.")
-    data_list = sock.recv(1024).decode().split("-")
-    data_list.pop(len(data_list) - 1)
-    print("List of Files: ")
-    for index, value in enumerate(data_list):
-        print(index, ".", value)
-        print("-----------------------")
 
-    INDEX = str(input("\nchoose number of file you want to download: "))
+def connect_to_server():
+    """Connect to server."""
+    sock.sendall(b"get-list")
+    data = sock.recv(1024)
+    if not data:
+        return
+    decoded = data.decode().split("-")
+    for item in decoded:
+        listbox.insert(tk.END, item)
 
-    sock.send(INDEX.encode())
-    recv_data = sock.recv(1024).decode()
-    file_name = recv_data
-    with open(file_name, "wb") as file:
-        line = sock.recv(1024)
-        while line:
-            file.write(line)
-            line = sock.recv(1024)
 
-        print("File has been received successfully.")
-    answer = input("you want to download again?(y/n)").lower()
-    sock.close()
+def download_file():
+    """Download file."""
+    file_index = listbox.curselection()[0]
 
-    if answer == "n":
-        print("Connection Closed.")
-        break
+    # filename = listbox.get(index)
+    # print('file', filename,index,)
+
+    sock.sendall(str(file_index).encode())
+    recv_filename = sock.recv(1024).decode()
+    size = int(sock.recv(1024).decode())
+    received = 0
+
+    with open(recv_filename, "wb") as file:
+        while received < size:
+            data = sock.recv(1024)
+            received += len(data)
+            file.write(data)
+    # with open(recv_filename, "wb") as file:
+    #     line = sock.recv(1024)
+    #     while line:
+    #         file.write(line)
+    #         line = sock.recv(1024)
+
+    # sock.close()
+    # listbox.delete(file_index)
+
+
+def upload():
+    """Upload to server."""
+    print("pressed")
+
+
+root = tk.Tk()
+
+notebook = ttk.Notebook(root)
+notebook.pack(fill="both", expand=True)
+
+download_frame = ttk.Frame(notebook)
+notebook.add(download_frame, text="Download")
+
+lbl_download = ttk.Label(download_frame, text="Download File:")
+btn_download = ttk.Button(
+    download_frame,
+    text="Download",
+    command=download_file,
+)
+listbox = tk.Listbox(download_frame)
+
+lbl_download.pack(pady=10)
+listbox.pack()
+btn_download.pack()
+
+connect_to_server()
+
+upload_frame = ttk.Frame(notebook)
+notebook.add(upload_frame, text="Upload")
+
+lbl_upload = ttk.Label(upload_frame, text="Select File to Upload:")
+btn_upload = ttk.Button(upload_frame, text="Upload", command=upload)
+
+lbl_upload.pack(pady=10)
+btn_upload.pack()
+
+root.mainloop()
--- src/server.py
+++ src/server.py
@@ -8,54 +8,57 @@ sock = socket.socket()
 print("Socket created successfully.")
 PORT = 8800
 HOST = ""
-
 sock.bind((HOST, PORT))
-sock.listen()
-print("Socket is listening...")
-# text = os.path.join("src", "assets", "text.txt")
-# video = os.path.join("src", "assets", "video.mp4")
-# image = os.path.join("src", "assets", "image.png")
-# sound = os.path.join("src", "assets", "sound.mp3")
+
 src = os.walk("src/assets")
 src_list = []
 for item in src:
     for value in item[2]:
         # print(idex)
         src_list.append(os.path.join("src", "assets", value))
+
 PATH = "".join(f"{value}-" for value in src_list)
 
-print(PATH)
 
-
-def send(soc, data):
-    """Send."""
-    soc.send(str(data).encode())
-    # try:
-    #     soc.send(str(data).encode())
-    # except Exception:
-    #     soc.send("error to send data form sever!".encode())
-
-
-def threaded(client):
-    """Threaded."""
-    send(client, PATH)
-    index = int(client.recv(1024).decode())
-    print(index)
-    file_name = os.path.basename(src_list[index])
-    send(client, file_name)
-    with open(src_list[index], "rb") as file:
-        line = file.read(1024)
-        while line:
-            send(client, line)
+def handle_client(client):
+    """Handel client."""
+    print("Connected")
+    while True:
+        print("wait for client")
+        data = client.recv(1024)
+        print(data.decode())
+        if not data:
+            break
+        if "get-list" in data.decode():
+            client.sendall(PATH.encode())
+            continue
+        index = int(data.decode())
+        file_name = os.path.basename(src_list[index])
+        client.sendall(str(file_name).encode())
+        size = os.path.getsize(src_list[index])
+        print(size)
+        client.send(str(size).encode())
+        with open(src_list[index], "rb") as file:
             line = file.read(1024)
+            while line:
+                client.sendall(line)
+                line = file.read(1024)
+                print(line)
+        # client.sendall(b'end')
 
-    print("File has been transferred successfully.")
+        print("File has been transferred successfully.")
+
     client.close()
 
 
+sock.listen()
+print("Listening...")
 for number in range(100):
     print(number)
-    client_soc, addr = sock.accept()
-    print("Connected with ", addr)
-    threading.Thread(target=threaded, args=(client_soc,)).start()
+    client_soc, address = sock.accept()
+    threading.Thread(target=handle_client, args=(client_soc,)).start()
 sock.close()
+# while True:
+#     client_soc, address = sock.accept()
+#     threading.Thread(target=handle_client, args=(client_soc,)).start()
+# sock.close()
